b92e8209b256615200875f09f2e1eb5021c001fa
Ken Masumoto <kenmasumoto@Kens-MacBook-Pro.local>
2019-11-22 19:03:55 -0800

    Created forward function and initialized empty probability table

--- forward.py
+++ forward.py
@@ -4,3 +4,13 @@ Authors:
     bowerw2
 This is our implementation of the forward algorithm for part of speech tagging.
 """
+
+
+def forward (observations, states, initial_prob, transition_prob, emission_prob):
+    probabilities = []
+    for i in range(len(states)):
+        row = []
+        for j in range(len(observations)):
+            row.append(0)
+        probabilities.append(row)
+        
