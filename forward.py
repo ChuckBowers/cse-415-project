b62d6b648fdae5ece41ce261016194d7a0d363e5
Ken Masumoto <kenmasumoto@Kens-MacBook-Pro.local>
2019-11-23 19:55:48 -0800

    Forward algorithm works and generates a cute little table

--- forward.py
+++ forward.py
@@ -6,10 +6,52 @@ This is our implementation of the forward algorithm for part of speech tagging.
 """
 
 
-def forward (observations, states, initial_prob, transition_prob, emission_prob):
+possible_states = ['sunny', 'rainy']
+states_i = [0, 1]
+possible_observations = ['umbrella', 'no umbrella']
+observed = [0, 0]
+emissions = [[0.2, 0.8], [0.9, 0.1]]  # [state][observation]
+transitions = [[0.7, 0.3], [0.3, 0.7]]  # [state 1][state 2]
+initial = [0.5, 0.5] # initial observations
+
+
+def forward(observations, states, initial_prob, transition_prob, emission_prob):
     probabilities = []
+
+    # Create a results tables populated with 0.0
     for i in range(len(states)):
         row = []
         for j in range(len(observations)):
-            row.append(0)
+            row.append(0.0)
         probabilities.append(row)
+
+    # Populate the first observation with the initial probability * the probability given the obs
+    for state in states:
+        probabilities[state][0] = initial_prob[state] * emission_prob[state][observations[0]]
+
+    # Populate the rest of the observations with the forward algorithm
+    for i in range(1, len(observations)):
+        for t1 in states:
+            probability = 0
+            for t2 in states:
+                probability += probabilities[t2][i - 1] * transition_prob[t1][t2]
+            probabilities[t1][i] = probability * emission_prob[t1][observations[i]]
+    return probabilities
+
+
+def format(result, poss_states, states_index):
+    print("\t", end="")
+    for i in range(len(result[0])):
+        print("\t", end="")
+        print("Obs", end="")
+        print(i + 1, end="")
+    print("")
+    for index in states_index:
+        print(poss_states[index], end="")
+        for i in range(len(result[0])):
+            print("\t", end="")
+            print("{0:.4f}".format(result[index][i]), end="")
+        print("")
+
+
+format(forward(observed, states_i, initial, transitions, emissions), possible_states, states_i)
